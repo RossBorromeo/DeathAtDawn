09cc3160769be0eb877a64b922526b3ed8e5c917
XxJT420xX <D00238448@student.dkit.ie>
2023-11-22 18:47:37 +0000

    Got sprinting
    Updated movement to where when you press left Shift the speed is increased

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -7,11 +7,22 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
-    public float speed = 6f;
+    public float walkspeed;
+    public float sprintspeed;
+    private float truespeed;
 
     public float turnSmoothTime = 0.1f;
 
     float turnSmoothVelocity;
+
+
+    private void Start()
+    {
+        truespeed = walkspeed;
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     // Update is called once per frame
     void Update()
@@ -20,14 +31,25 @@ public class ThirdPersonMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3 (horizontal, 0, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f)
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            truespeed = sprintspeed;
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            truespeed = walkspeed;
+        }
+
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move (moveDirection.normalized * speed * Time.deltaTime);
+            controller.Move (moveDirection.normalized * truespeed * Time.deltaTime);
         }
     }
 }
