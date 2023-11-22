a320381e016c58d6ae183f95fae38d4064758931
XxJT420xX <D00238448@student.dkit.ie>
2023-11-22 19:02:14 +0000

    Update movement jump
    Still not sure why character starts in the air and not attached to ground. Got jumping and gravity but not working fully.

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
+
+    //Essentials
     public CharacterController controller;
     public Transform cam;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
+
+    //Movement
     public float walkspeed;
     public float sprintspeed;
     private float truespeed;
 
-    public float turnSmoothTime = 0.1f;
 
-    float turnSmoothVelocity;
 
+
+
+
+    //Jumping
+    public float jumpHeight;
+    public float gravity;
+    bool isGrounded;
+    Vector3 velocity;
 
     private void Start()
     {
@@ -27,9 +39,14 @@ public class ThirdPersonMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3 (horizontal, 0, vertical).normalized;
+
+        isGrounded = Physics.CheckSphere(transform.position, .1f, 1);
+
+        if(isGrounded && velocity.y < 0) 
+        {
+            velocity.y = -1;
+        
+        }
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
@@ -42,6 +59,10 @@ public class ThirdPersonMovement : MonoBehaviour
             truespeed = walkspeed;
         }
 
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3 (horizontal, 0, vertical).normalized;
+
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -51,5 +72,20 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move (moveDirection.normalized * truespeed * Time.deltaTime);
         }
+
+        //Jumping
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt((jumpHeight * 10) * -2f * gravity);
+        }
+
+        if(velocity.y > -20)
+        {
+            velocity.y += (gravity * 10) * Time.deltaTime;
+        }
+
+
+       
+        controller.Move(velocity * Time.deltaTime);
     }
 }
