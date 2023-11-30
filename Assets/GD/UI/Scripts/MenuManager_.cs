a5ca8b661c06f5401e5e480048e491816ab572f5
Dawood Parhiar <D00248313@student.dkit.ie>
2023-11-30 19:19:24 +0000

    Changes in the commit to merge UI branch to main

--- Assets/GD/UI/Scripts/MenuManager_.cs
+++ Assets/GD/UI/Scripts/MenuManager_.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour
+public class MenuManager_ : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject playButton;
+    public GameObject ControlsMenu;
+    public AudioSource soundSource;
+    public AudioClip soundClip;
+
+
+    public void Start()
     {
-        
+        playButton.SetActive(true);
+        ControlsMenu.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPlayButton()
     {
-        
+        PlaySound();
+
+        SceneManager.LoadScene(0);
+    }
+
+    private void PlaySound()
+    {
+        soundSource.clip = soundClip;
+        soundSource.Play(); //play sound
+    }
+
+    public void OnControlsNutton()
+    {
+        PlaySound();
+        soundSource.Play(); //play sound
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void OnQuitButton()
+    {
+        PlaySound();
+        playButton.SetActive(false);
+        ControlsMenu.SetActive(false);
+        Application.Quit();
     }
 }
