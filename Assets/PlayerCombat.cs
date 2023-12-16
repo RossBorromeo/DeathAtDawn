ef9f2c33feb45d7a06595d44314a78d3e35aee19
XxJT420xX <D00238448@student.dkit.ie>
2023-12-16 23:46:16 +0000

    AI and Animations
    Enemy AI coding player attack animation done. Fixing animations

--- Assets/PlayerCombat.cs
+++ Assets/PlayerCombat.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 50;
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+       
+    }
+
+
+
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+
+        Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
+
+
+        foreach(Collider enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
